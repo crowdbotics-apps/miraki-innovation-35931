dc8f5fe44b1ad1f46815115f7b2eae2494aaf25f
Crowdbotics <team@crowdbotics.com>
2022-06-27 17:59:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "miraki_innovation_35931",
+  url: "https://miraki_innovation_35931.botics.co",
+  api: "https://miraki_innovation_35931.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
